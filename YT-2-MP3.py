5e0f4de14aa6629c5e42b03b7e850d5b368c2c91
Sylph1de <88037480+Sylph1de@users.noreply.github.com>
2022-07-07 09:29:52 -0300

    Add files via upload

---
+++ YT-2-MP3.py
@@ -0,0 +1,87 @@
+import pytube as pt
+import pandas as pd
+import os
+
+path = os.path.join(os.environ["USERPROFILE"], "Videos")
+# path = 'D:\Videos'
+
+def download():
+    link_search = input('Your YouTube Video/Playlist/Channel/Title: ')
+
+    if 'https://www.youtube.com/watch?' in link_search:
+        index_list = []
+        yt = pt.YouTube(link_search)
+        selection = yt.streams.filter(only_audio=True, subtype='mp4').order_by('abr').last()
+        
+        file_name = yt.title + '.mp3'
+
+        download_path = selection.download(filename=file_name,output_path = path)
+        
+        print('')
+        print(f'Audio downloaded to: "{download_path}"')
+        print('')
+        
+    # elif 'https://www.youtube.com/playlist?' in link_search:
+    #     playlist = pt.Playlist(link_search)
+        
+    #     video_list = playlist.videos
+        
+    #     for video in video_list:
+    #         file_name = video.title + '.mp3'
+    #         download_path = video.streams.filter(only_audio=True, subtype='mp4').order_by('abr').last().download(filename=file_name,output_path = path)
+    #         print('')
+    #         print(f'Audio downloaded to: "{download_path}"')
+    #         print('')
+        
+    elif 'https://www.youtube.com/channel/' in link_search or 'https://www.youtube.com/c/' in link_search:
+        channel = pt.Channel(link_search)
+        
+        video_list = channel.videos
+        
+        for video in video_list:
+            file_name = video.title + '.mp3'
+            download_path = video.streams.filter(only_audio=True, subtype='mp4').order_by('abr').last().download(filename=file_name,output_path = path)
+            print('')
+            print(f'Audio downloaded to: "{download_path}"')
+            print('')
+
+    else:
+        index_list = []
+        
+        search = pt.Search(link_search)
+        dicc = {'Title': [],
+                'Author': [],
+                'URL': [] 
+                }
+        
+        for index,result in enumerate(search.results):
+            rs = vars(result)
+            index_list.append(index + 1)
+            dicc['Title'].append(rs.get('_title'))
+            dicc['Author'].append(rs.get('_author'))
+            dicc['URL'].append(rs.get('watch_url'))
+                
+        df = pd.DataFrame(dicc, index=index_list)
+        
+        print('')
+        print(df)
+        print('')
+        
+        selection = int(input('Please select your video by index: '))
+        print('')
+        
+        yt = pt.YouTube(df['URL'][selection])
+        
+        selection = yt.streams.filter(only_audio=True, subtype='mp4').order_by('bitrate').last()
+    
+        file_name = yt.title + '.mp3'
+
+        download_path = selection.download(filename=file_name,output_path = path)
+        
+        print('')
+        print(f'Audio downloaded to: "{download_path}"')
+        print('')
+    
+    download()
+    
+download()
